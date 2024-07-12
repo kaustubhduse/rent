c1100dcb8970f9a8e1adf58ca6164403ed6b9ae7
kaustubhduse <202251045@iiitvadodara.ac.in>
2024-07-12 14:06:33 +0530

    Add new components: AddPhotos and OtherComponent

--- src/components/Preview/ImageSection.js
+++ src/components/Preview/ImageSection.js
@@ -4,25 +4,25 @@ import './ImageSection.css';
 const ImageSection = ({ children }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % children.length);
-  };
+  // const goToNext = () => {
+  //   setCurrentIndex((prevIndex) => (prevIndex + 1) % children.length);
+  // };
 
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + children.length) % children.length);
-  };
+  // const goToPrevious = () => {
+  //   setCurrentIndex((prevIndex) => (prevIndex - 1 + children.length) % children.length);
+  // };
 
   return (
     <div className='all-images'>
-      <button className='prev-button' onClick={goToPrevious}>
+      {/* <button className='prev-button' onClick={goToPrevious}>
         &lt;
-      </button>
+      </button> */}
       <div className='image-container'>
         {children[currentIndex]}
       </div>
-      <button className='next-button' onClick={goToNext}>
+      {/* <button className='next-button' onClick={goToNext}>
         &gt;
-      </button>
+      </button> */}
     </div>
   );
 };
